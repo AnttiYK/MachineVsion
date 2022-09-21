0f4da0931a9f955bb318868d9a1cca60957e8119
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-09-21 15:31:27 +0300

    Modified median_filter.m

--- median_filter.m
+++ median_filter.m
@@ -1,14 +1,4 @@
 function J = median_filter(I, K)
-% AUTHOR: Antti Yli-Kujala
-% STUDENT NUMBER: e123107
-% 
-% Apply median filter to image I using a window of size KxK
-% I can be either grayscale or color image. K is assumed to be odd.
-% PARAMETERS:
-% I: greyscale or rgb image
-% K: size of the neighbourhood window. Must be odd
-% OUTPUT:
-% J: filtered image, same size of I
 
 [r,c] = size(I);
 for i = 1 : r 
